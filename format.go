f25a0a94805e06f9e69f62995003d1d117494d43
Tom Payne <twpayne@gmail.com>
2019-06-10 16:05:19 +0200

    Add PNG color maps

--- format.go
+++ format.go
@@ -46,6 +46,54 @@ var (
 		formatString: "png",
 		contentType:  "image/png",
 	}
+	FormatPNGDefault = Format{
+		formatString: "png_default",
+		contentType:  "image/png",
+	}
+	FormatPNGJet = Format{
+		formatString: "png_jet",
+		contentType:  "image/png",
+	}
+	FormatPNGJetSegmented = Format{
+		formatString: "png_jet_segmented",
+		contentType:  "image/png",
+	}
+	FormatPNGBlueToRed = Format{
+		formatString: "png_blue_to_red",
+		contentType:  "image/png",
+	}
+	FormatPNGBlueMagenta = Format{
+		formatString: "png_blue_magenta",
+		contentType:  "image/png",
+	}
+	FormatPNGBlues = Format{
+		formatString: "png_blues",
+		contentType:  "image/png",
+	}
+	FormatPNGGray = Format{
+		formatString: "png_gray",
+		contentType:  "image/png",
+	}
+	FormatPNGPeriodic = Format{
+		formatString: "periodic",
+		contentType:  "image/png",
+	}
+	FormatPNGPlasma = Format{
+		formatString: "png_plasma",
+		contentType:  "image/png",
+	}
+	FormatPNGPrism = Format{
+		formatString: "png_prism",
+		contentType:  "image/png",
+	}
+	FormatPNGReds = Format{
+		formatString: "png_reds",
+		contentType:  "image/png",
+	}
+	FormatPNGSeismic = Format{
+		formatString: "png_seismic",
+		contentType:  "image/png",
+	}
 	FormatXML = Format{
 		formatString: "xml",
 		contentType:  "application/xml",
